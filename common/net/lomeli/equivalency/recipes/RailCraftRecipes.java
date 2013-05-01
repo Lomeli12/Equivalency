e095ee9b48a59f4640422cc9cb0fcaaf8ec037eb
Lomeli12 <anthonylomeli11@gmail.com>
2013-05-01 00:56:27 -0700

    Railcraft transmutation now works thanks to some help from CovertJaguar.

--- common/net/lomeli/equivalency/recipes/RailCraftRecipes.java
+++ common/net/lomeli/equivalency/recipes/RailCraftRecipes.java
@@ -1,39 +1,44 @@
 package net.lomeli.equivalency.recipes;
 
-import cpw.mods.fml.common.registry.GameRegistry;
+import java.util.List;
 
 import net.lomeli.equivalency.helper.TransmutationHelper;
 import net.lomeli.equivalency.mods.ic2.AddonIC2;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class RailCraftRecipes 
 {
 	public static final int WILDCARD = Short.MAX_VALUE;
-	private static final String MODID = "Railcraft";
 	
-	public static Item steelIngot = GameRegistry.findItem(MODID, "ingot.Steel");
-	public static Item coalCoke = GameRegistry.findItem(MODID, "fuel.Coke");
+	public static List<ItemStack> steelIngot = OreDictionary.getOres("ingotSteel");
+	public static List<ItemStack> coalCoke = OreDictionary.getOres("fuelCoke");
 	
 	public static void loadRecipes(ItemStack transmutationStone)
 	{
-		// 3 Coal/CharCoal -> 2 Coal Coke
-		TransmutationHelper.addRecipe(new ItemStack(coalCoke, 2), transmutationStone,
-			new Object[]{ (new ItemStack(Item.coal, 1, WILDCARD)), (new ItemStack(Item.coal, 1, WILDCARD)),
-			(new ItemStack(Item.coal, 1, WILDCARD)) });
-		// 2 Coal Coke -> 3 Coal/CharCoal
-		TransmutationHelper.addRecipe(new ItemStack(Item.coal, 3), transmutationStone,
-			new Object[]{ coalCoke, coalCoke, coalCoke});
-		
-		if(AddonIC2.checkIC2())
+		for(ItemStack steel : steelIngot)
 		{
-			// 2 Refined -> 2 Steel
-			TransmutationHelper.addRecipe(new ItemStack(steelIngot, 2), 
-				transmutationStone, new Object[]{ IC2Recipes.refinedIronIngot, IC2Recipes.refinedIronIngot });
-			// 2 Steel -> 2 Refined Iron
-			TransmutationHelper.addRecipe(new ItemStack(IC2Recipes.refinedIronIngot.getItem(), 2), 
-				transmutationStone, new Object[]{ steelIngot, steelIngot });
+			if(AddonIC2.checkIC2())
+			{
+				// 2 Refined Iron -> 2 Steel
+				TransmutationHelper.addRecipe(new ItemStack(steel.getItem(), 2), 
+					transmutationStone, new Object[]{ IC2Recipes.refinedIronIngot, IC2Recipes.refinedIronIngot });
+				// 2 Steel -> 2 Refined Iron
+				TransmutationHelper.addRecipe(new ItemStack(IC2Recipes.refinedIronIngot.getItem(), 2), 
+					transmutationStone, new Object[]{ steel, steel });
+			}
+		}
+		for(ItemStack coke : coalCoke)
+		{
+			// 2 Coal/CharCoal -> 1 Coal Coke
+			TransmutationHelper.addRecipe(coke, transmutationStone,
+				new Object[]{ (new ItemStack(Item.coal, 1, WILDCARD)),
+				(new ItemStack(Item.coal, 1, WILDCARD)) });
+			// 1 Coal Coke -> 2 Coal/CharCoal
+			TransmutationHelper.addRecipe(new ItemStack(Item.coal, 2), transmutationStone,
+				new Object[]{ coke });
 		}
 	}
 }
